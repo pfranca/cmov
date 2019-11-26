0453cd49b930e2100629065933221088294faf12
1120669 <1120669@isep.ipp.pt>
2019-11-26 17:54:01 +0000

    db creds

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 //Create Connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'cmovdb'
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b7ecfb994f5dae',
+  password: '87e7b9f4',
+  database: 'heroku_64569440f50a48b'
 });
 
 module.exports = db;
